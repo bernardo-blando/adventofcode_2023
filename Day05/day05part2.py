fb4787f451455702869f45216c3df7d5b794c0d6
bernardo-blando <bernardoblando@gmail.com>
2023-12-08 20:31:53 +0100

    day5part2

--- Day05/day05part2.py
+++ Day05/day05part2.py
@@ -16,30 +16,46 @@ class Function:
   """
   def __init__(self, intervals):
     self.intervals = intervals
-
-
-  def interval_response(self, start, stop):
+  
+  def interval_response_v2(self, start, stop):
     """
     Returns the response of the function for the input interval.
-    Output: List of lists describing the funcions response for the input interval.
+    Output: List of lists describing the function's response for the input interval.
     """
     response = []
     for i in range(len(self.intervals)):
+
+      # Check if the interval is completely within the input interval
       if self.intervals[i][0] >= start and self.intervals[i][1] <= stop:
-        response.append([self.intervals[i][0], self.intervals[i][1], self.intervals[i][2]])
-      elif self.intervals[i][0] >= start and self.intervals[i][0] < stop:
-        response.append([self.intervals[i][0], stop, self.intervals[i][2]])
-      elif self.intervals[i][1] > start and self.intervals[i][1] <= stop:
-        response.append([start, self.intervals[i][1], self.intervals[i][2]])
+        print("Case 1", self.intervals[i], "vs", [start, stop])
+        response.append([self.intervals[i][0]+ self.intervals[i][2], self.intervals[i][1] + self.intervals[i][2]])
+
+      # Check if the interval starts within the input interval but ends outside
+      elif (self.intervals[i][0] >= start and self.intervals[i][0] <= stop) and self.intervals[i][1] > stop:
+        print("Case 2", self.intervals[i], "vs", [start, stop])
+        response.append([self.intervals[i][0]+self.intervals[i][2], stop+self.intervals[i][2]])
+
+      # Check if the interval ends within the input interval but starts outside
+      elif self.intervals[i][0] < start and self.intervals[i][1] >= start and self.intervals[i][1] <= stop:
+        print("Case 3", self.intervals[i], "vs", [start, stop])
+        response.append([start+self.intervals[i][2], self.intervals[i][1]+self.intervals[i][2]])
+
+      # Check if the interval starts before the input interval and ends after
       elif self.intervals[i][0] < start and self.intervals[i][1] > stop:
-        response.append([start, stop, self.intervals[i][2]])
+        print("Case 4", self.intervals[i], "vs", [start, stop])
+        response.append([start+self.intervals[i][2], stop+self.intervals[i][2]])
+
     return response
+    
   
   def fill_interval_gaps(self):
     """
     Updates the intervals of the function by filling the gaps between them with [start, stop, 0]
     Output: None
     """
+    #Ensure that self.intervals is sorted
+    self.intervals = sorted(self.intervals, key=lambda x: x[0])
+
     response = []
     for i in range(len(self.intervals)-1):
       response.append([self.intervals[i][1]+1, self.intervals[i+1][0]-1, 0])
@@ -65,29 +81,37 @@ class Function:
     response = sorted(response, key=lambda x: x[2])
     return response[0][2]+response[0][0]
  
-
   def composite_function(self, other):
     """
     Returns the composite function of self and other.
     Output: A new Function object describing the composite function. 
     """
     response = []
-    # print("Self intervals", self.intervals)
-    # print("Other intervals", other.intervals)
     for i in other.intervals:
-      afected_interval = [i[0]+i[2], i[1]+i[2]]
-      # print("Afected interval", afected_interval)
-      self_function_response = self.interval_response(afected_interval[0], afected_interval[1])
-      # print("Self function response", self_function_response)
+      affected_start = min(i[0], i[1])
+      affected_stop = max(i[0], i[1])
+      self_function_response = self.interval_response(affected_start, affected_stop)
       for j in self_function_response:
-        response.append([j[0]-i[2], j[1] -i[2], j[2]+i[2]])
+        response.append([j[0] - i[2], j[1] - i[2], j[2] + i[2]])
 
-    #sort response
+    # Sort response
     response = sorted(response, key=lambda x: x[0])
 
     return Function(response)
+
+  def remove_negative_intervals(self):
+    """
+    Removes the intervals with negative y values.
+    Output: None
+    """
+    response = []
+    for i in self.intervals:
+      if i[1] > i[0]:
+        response.append(i)
+    self.intervals = response
     
-def find_function(data, response=[]):
+def find_function(data):
+  response=[]
   df = pd.DataFrame(data, columns=['destination', 'source', 'range'])
   df = df.sort_values('source')
   response.append([-math.inf, df['source'].min() -1, 0])
@@ -96,9 +120,27 @@ def find_function(data, response=[]):
     stop = df.iloc[i]['source']+df.iloc[i]['range']-1
     operation = df.iloc[i]['destination']- df.iloc[i]['source']
     response.append([start, stop, operation])
+  response.sort(key=lambda x: x[0])
   response.append([response[-1][1]+1, math.inf, 0])
   return response
     
+def clean_data(input_data):
+  input_data = input_data.split('\n\n')
+  input_data = [group.split('\n') for group in input_data]
+  input_data[-1].pop(-1)
+
+  seeds = input_data.pop(0)[0].split()[1:]
+  seeds = [int(seed) for seed in seeds]
+
+  data = {}
+  for group in input_data:
+    key = group[0][:-5]
+    values = [list(map(int, item.split())) for item in group[1:]]
+    data[key] = values
+
+  new_seeds = [seeds[i:i+2] for i in range(0, len(seeds), 2)]
+
+  return data, new_seeds
 
 load_dotenv()
 
@@ -107,77 +149,35 @@ url = f'https://adventofcode.com/2023/day/5/input'
 response = requests.get(url, headers={"Cookie": f"session={COOKIE}"})
 input_data = response.text
 
-# print(input_data)
+print(input_data)
 
 # with open('data.txt', 'r') as f:
 #   input_data = f.read()
 
-input_data = input_data.split('\n\n')
-for i in range(len(input_data)):
-  input_data[i] = input_data[i].split('\n')
+data, seeds = clean_data(input_data)
 
-input_data[-1].pop(-1)
+functions={}
+for key in data.keys():
+  functions[key] = Function(find_function(data[key]))
 
-seeds = input_data.pop(0)
-seeds = seeds[0].split(' ')
-seeds = seeds[1:]
-seeds= [int(i) for i in seeds]
-seeds = Type('seed', seeds)
+seeds = [[x[0], x[1] + x[0]-1] for x in seeds]
 
-data = {}
-for i in range(len(input_data)):
-  input_data[i][0] = input_data[i][0][0:-5]
-  data[input_data[i][0]] = [x.split(' ') for x in input_data[i][1:]]
-  data[input_data[i][0]] = [[int(x) for x in y] for y in data[input_data[i][0]]]
+print("Seeds1", seeds)
+for name, function in functions.items():
+  print(name, function.intervals)
+  results_for_function = []
+  for seed in seeds:
+    print("Seed", seed)
+    seed_result = function.interval_response_v2(seed[0], seed[1])
+    results_for_function.extend(seed_result)
 
-functions = []
-for key, val in data.items():
-  functions.append(Function(find_function(val, response=[])))
+  sorted_results = sorted(results_for_function, key=lambda x: x[0])  
+  print("Results for function", name, sorted_results)
 
-print("Function  0",functions[0].intervals)
-print("Function 1:", functions[1].intervals)
-print("Function 2:", functions[2].intervals)
-print("Function 3:", functions[3].intervals)
-print("Function 4:", functions[4].intervals)
-print("Function 5:", functions[5].intervals)
-print("Function 6:", functions[6].intervals)
-
-final_function = functions[1].composite_function(functions[0])
-print("Final Function: 1 o 0", final_function.intervals)
-
-final_function = functions[2].composite_function(final_function)
-print("Final Function: 2 o 1 o 0", final_function.intervals)
-
-final_function = functions[3].composite_function(final_function)
-print("Final Function: 3 o 2 o 1 o 0", final_function.intervals)
-
-final_function = functions[4].composite_function(final_function)
-print("Final Function: 4 o 3 o 2 o 1 o 0", final_function.intervals)
-
-final_function = functions[5].composite_function(final_function)
-print("Final Function: 5 o 4 o 3 o 2 o 1 o 0", final_function.intervals)
-
-final_function = functions[6].composite_function(final_function)
-
-final_function.fill_interval_gaps()
-
-print("Final Function: 6 o 5 o 4 o 3 o 2 o 1 o 0", final_function.intervals)
+  seeds = sorted_results
 
 
 
-new_seeds = []
-#group seed values in pairs by order
-for i in range(0, len(seeds.values), 2):
-  new_seeds.append([seeds.values[i], seeds.values[i+1]])
 
-print("New seeds", new_seeds)
 
-lowest_values = []
-for i in new_seeds:
-  print("Seed pair", i)
-  lowest_values.append(final_function.lowest_response_for_interval(i[0], i[1]+i[0]))
-  print("Lowest value of the pair", lowest_values[-1])
-
-print("LOWEST VALUE", min(lowest_values))
-
-print()
+print(seeds[0][0])
